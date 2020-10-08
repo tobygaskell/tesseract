b76ec12801281be3a12ce9032a7a9af92b1da4d8
tobygaskell <toby96@sky.com>
2020-10-08 18:06:43 +0100

    ensure tom has most up to date version

--- test.py
+++ test.py
@@ -145,11 +145,14 @@ def pull(url):
     return data 
 
 def display_data(dict):
+    """
+    """
     print(json.dumps(dict, indent = 1))
 
 
 def print_results(raw_data):
-
+    """
+    """
     for i in raw_data: 
         print(i['homeTeam']['team_name'], end = ' ') 
         print(i['goalsHomeTeam'], 'vs', i['goalsAwayTeam'], end = ' ' ) 
@@ -157,6 +160,8 @@ def print_results(raw_data):
 
 
 def find_teams_that_played(raw_data): 
+    """
+    """
     played  = [i for i in raw_data if i['status'] == 'Match Finished']
     not_played = []
     for i in raw_data: 
@@ -166,6 +171,8 @@ def find_teams_that_played(raw_data):
     return played, not_played 
 
 def save_results(played, round_number): 
+    """
+    """
     data = {'team':[],'apponent': [],'result': [],'score': []} 
     for i in played: 
         data['team'].append(i['homeTeam']['team_name'])
@@ -181,6 +188,8 @@ def save_results(played, round_number):
 
 
 def get_result(team_name, round_number):
+    """
+    """
     winners, loosers, draws = get_team_lists(round_number)[:3]
     if team_name in winners: 
         result = 'winner'
@@ -193,6 +202,8 @@ def get_result(team_name, round_number):
     return result 
 
 def remove_draws(played, draws): 
+    """
+    """
     played_ = []
     for i in played: 
         if i['homeTeam']['team_name'] not in draws: 
@@ -201,7 +212,9 @@ def remove_draws(played, draws):
     return played_
 
 
-def find_winners_and_loosers(wins_and_loss): 
+def find_winners_and_loosers(wins_and_loss):
+    """
+    """ 
     winners = [i['homeTeam']['team_name'] if i['goalsHomeTeam'] > i['goalsAwayTeam'] 
                                           else i['awayTeam']['team_name'] for i in wins_and_loss]
     loosers = [i['homeTeam']['team_name'] if i['goalsHomeTeam'] < i['goalsAwayTeam'] 
@@ -210,6 +223,8 @@ def find_winners_and_loosers(wins_and_loss):
 
 
 def find_draws(played): 
+    """
+    """
     draws = []
     for i in played: 
 
@@ -220,37 +235,65 @@ def find_draws(played):
 
 
 def find_points(round_number, choices): 
+    """
+    """
     points = {}
+    DP = DP_round()
+    DW = draw_weekend(round_number)
     winners, loosers, draws, not_played = get_team_lists(round_number)[:4]
     for name, choice in choices.items(): 
         if choice in winners: 
-            value = winners_round_worth(choice, choices, round_number) 
+            value = round_worth(1, choice, choices, round_number, DP) 
         elif choice in loosers: 
-            value = loosers_round_worth()
+            value = round_worth(-1, choice, choices, round_number, DP)
         elif choice in draws: 
-            value = draws_round_worth()
+            value = draws_round_worth(0, choice, round_number, DW)
         elif choice in not_played: 
             value = 0 
         else: 
             value = 0
 
         points[name] = value
-    return points
+    return points, DP, DW
+
 
 def DP_round(): 
+    """
+    """
     return random.randrange(10) > 8  
 
-def winners_round_worth(choice, choices, round_number ): 
-    double_points = DP_round()
-    derby = played_in_a_derby(choice, choices) 
+def draw_weekend(round_number): 
+    """
+    """
+    return random.randrange(100) > 80
+
+
+
+def round_worth(value, choice, choices, round_number, double_points): 
+    """
+    """
+    derby = played_in_a_derby(choice, round_number) 
     head_to_head = played_head_to_head(choice, choices, round_number) 
     if double_points == True: 
-        value = 2 
+        value *= 2 
     if head_to_head == True: 
-        value = 2
+        value *= 2
     if derby == True: 
-        value = 2
+        value *= 2
     return value  
+
+
+def draws_round_worth(value, choice, round_number, draw_weekend):
+    """
+    """
+    derby = played_in_a_derby(choice, round_number)
+    if derby == True: 
+        value = -1 
+    if draw_weekend == True:
+        if value == 0: 
+            value = 1 
+        value *= 3 
+    return value
 
 
 def played_in_a_derby(choice, round_number): 
@@ -272,7 +315,6 @@ def played_in_a_derby(choice, round_number):
     return derby 
 
 
-
 def played_head_to_head(choice, choices, round_number):
     """
     """
@@ -283,13 +325,10 @@ def played_head_to_head(choice, choices, round_number):
         head_to_head = False
     return head_to_head
 
-def loosers_round_worth():
-    return -1 
-
-def draws_round_worth(): 
-    return 0 
 
 def get_team_lists(round_number): 
+    """
+    """
     round = f'Regular_Season_-_{round_number}'
     data = pull(f"https://api-football-v1.p.rapidapi.com/v2/fixtures/league/2790/{round}")
     played, not_played = find_teams_that_played(data['api']['fixtures'])
@@ -300,23 +339,20 @@ def get_team_lists(round_number):
 
 
 def main(round_number): 
+    """
+    """
     choices = read_choices()
-    #initilize_scores(choices)
-    points = find_points(round_number, choices)
+    if round_number == '1': 
+        initilize_scores(choices)
+    points, double_weekend, draw_weekend = find_points(round_number, choices)
     points = pd.DataFrame(points, index = ['Points']).T
     save_points(points, round_number)
     scores = update_overall_scores(round_number)
     save_scores(scores)
+    print('Draw Weekend = ', draw_weekend)
+    print('Double Weekend = ', double_weekend)
 
 
-
-if __name__ == '__main__': 
+if __name__ == "__main__": 
     round_number = input('round? ')
-    choices = read_choices() 
-    for i in choices.values(): 
-        print(played_in_a_derby(i, round_number))
-
-
-'''if __name__ == "__main__": 
-    round_number = input('round? ')
-    main(round_number)'''
+    main(round_number)
